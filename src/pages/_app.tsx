e8d296ce1e29a8e83e52b8ba261bd98dde689b99
André Borges <borges.an@hotmail.com>
2023-02-04 18:38:40 -0300

    up Code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <div>
-      teste
+    <Container>
+      <Header>
+        <Image 
+          src={logoImg.src} 
+          alt="" 
+          width={40}
+          height={40}
+        />
+      </Header>
       <Component {...pageProps} />
-    </div>
+    </Container>
     
   )
 }
