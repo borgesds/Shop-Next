3910c81d49207a5324a97d54e0e37f696f58cb3c
André Borges <borges.an@hotmail.com>
2023-02-04 13:35:29 -0300

    up Code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
 import { styled } from "../styles"
 
 const Button = styled('button', {
-  backgroundColor: '$rocketseat'
+  backgroundColor: '$rocketseat',
+  borderRadius: 6,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+    padding: '4px',
+  },
+
+  '&:hover': {
+    filter: 'brightness(90%)',
+  },
 })
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
   )
 }
