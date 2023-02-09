32eeb8c66eae9d3f39e00f77b59de04502c26ba5
André Borges <borges.an@hotmail.com>
2023-02-09 18:39:07 -0300

    up Code

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,21 @@
+import { stripe } from "@/src/lib/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    return res.json({message: 'Hello World!'})
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const priceId = ''
+
+    const successUrl = `${process.env.NEXT_URL}/Success`
+    const cancelUrl = `${process.env.NEXT_URL}/`
+
+    const checkoutSession = await stripe.checkout.sessions.create({
+        success_url: successUrl,
+        cancel_url: cancelUrl,
+        mode: 'payment',
+        line_items: [
+            {
+                price: priceId,
+                quantity: 1,
+            }
+        ]
+    })
 }
