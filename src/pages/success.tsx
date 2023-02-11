176acd6715877638f2bbc7feeac0a1deaf6b43d4
André Borges <borges.an@hotmail.com>
2023-02-11 19:41:52 -0300

    up Code

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,20 +1,79 @@
+import { url } from "inspector";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
-import { ImageContainer, SuccessContainer } from "../styles/pages/sucess";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
-export default function Success() {
+interface SuccessProps {
+    customerName: string
+    product: {
+        name: string
+        imageUrl: string
+    }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
     return (
-        <SuccessContainer>
-            <h1>Compra efetuada!</h1>
+        <>
+            <Head>
+                <title>Compra efetuada | iGUIS Shop</title>
 
-            <ImageContainer></ImageContainer>
+                <meta name="robots" content="noindex" />
+            </Head>
 
-            <p>
-                Uhuul <span>André Borges</span>, sua <span>Camiseta Astro Code</span> já está a caminho da sua casa.
-            </p>
+            <SuccessContainer>
+                <h1>Compra efetuada!</h1>
 
-            <Link href="/">
-                Voltar ao Catalogo
-            </Link>
-        </SuccessContainer>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={120} height={110} alt="" />
+                </ImageContainer>
+
+                <p>
+                    Uhuul <span>{customerName}</span>, sua <span>{product.name}</span> já está a caminho da sua casa.
+                </p>
+
+                <Link href="/">
+                    Voltar ao Catalogo
+                </Link>
+            </SuccessContainer>
+        </>
     )
+        
+        
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    // Redirecionar para a home se não tiver acesso
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            }
+        }
+    }
+
+    const sessionId = String(query.session_id)
+
+    if (!sessionId) {}
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+        expand: ['line_items', 'line_items.data.price.product']
+    })
+
+    const customerName = session.customer_details.name
+    const product = session.line_items.data[0].price.product
+
+    return {
+        props: {
+            customerName,
+            product: {
+                name: product.name,
+                imageUrl: product.images[0],
+            }
+        }
+    }
 }
